4734b760f971d250e49db08646a9efc19ff533ea
gazhonsepaskwa <gaz.honsepaskwa@gmail.com>
2024-10-18 12:50:29 +0200

    smal ajust

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: nalebrun <nalebrun@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:36:46 by nalebrun          #+#    #+#             */
-/*   Updated: 2024/10/18 09:21:17 by nalebrun         ###   ########.fr       */
+/*   Updated: 2024/10/18 12:46:18 by nalebrun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bpaszerro(void *s, size_t n)
+static void	ft_bpaszerro(void *s, size_t n)
 {
 	unsigned char	*ps;
 	size_t			i;
